4178aa117ad394a91826e281d019d258317b7a8e
ayshadairi <ayshadairi@gmail.com>
2023-12-26 13:09:49 -0330

    Add project files.

---
+++ overload operator/Employee.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+
+namespace overload_operator
+{
+    //created an Employee class with Id, First Name and Last Name properties
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+        //overload "==" operator to compare two Employee objects by their Id property
+        public static bool operator == (Employee employee1, Employee employee2)
+        {
+            if (ReferenceEquals(employee1, employee2))
+            {
+                return true;
+            }
+
+            if (ReferenceEquals(employee1, null) || ReferenceEquals(employee2, null))
+            {
+                return false;
+            }
+            return employee1.Id == employee2.Id;
+        }
+        public static bool operator != (Employee employee1, Employee employee2)
+        {
+            return !(employee1 == employee2);
+        }
+    }
+}
---
+++ overload operator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace overload_operator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate two Employee objects and compare using overload operators
+            Employee employee1 = new Employee
+            {
+                Id = 1,
+                FirstName = "Rula",
+                LastName = "Hussam"
+            };
+
+            Employee employee2 = new Employee
+            {
+                Id = 2,
+                FirstName = "John",
+                LastName = "Smith"
+            };
+
+            Console.WriteLine($"Employee 1 and Employee 2 are {(employee1 == employee2 ? "equal" : "not equal")}");
+
+            employee2.Id = 1;
+
+            Console.WriteLine($"Employee 1 and Employee 2 are {(employee1 == employee2 ? "equal" : "not equal")}");
+
+            Console.ReadLine();
+        }
+    }
+}
